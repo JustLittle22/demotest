2da81e3bd79a3e2911fe91613e3833bb79bfd570
JustLittle22 <nhatanhd96@gmail.com>
2022-08-18 08:07:22 +0700

    Chuyen đoi tien te tu USD sang VND

---
+++ String/Chuyen_doi_tien.py
@@ -0,0 +1,5 @@
+Tien_USD = int(input("Nhập số tiền USD cần chuyển đổi: "))
+Tien_VN = Tien_USD * 23000
+
+# print("Số tiền Việt Nam đã chuyển thành từ {} USD là:".format(Tien_USD), "{:,.0f}".format(Tien_VN), "Đồng")
+print("Số tiền Việt Nam đã chuyển thành từ {} USD là: {:,.0f} Đồng".format(Tien_USD,Tien_VN))
