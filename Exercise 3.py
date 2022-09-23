1da209c7d3f7d37466a3d4d3352ed63134061781
JustLittle22 <87955007+JustLittle22@users.noreply.github.com>
2022-09-23 14:58:48 +0700

    Get first, second best scores from the list.

---
+++ Exercise 3.py
@@ -0,0 +1,9 @@
+list_day_so = [20,55,44,85,96,75,21,3,5,7,65]
+
+# Sắp xếp theo thứ tự tăng dần
+list_day_so.sort()
+print("Hai số lớn nhất là: ", list_day_so[-1], 'và', list_day_so[-2])
+
+# Sắp xếp theo thứ tự giảm dần
+list_day_so.sort(reverse = True)
+print("Hai số lớn nhất là: ", list_day_so[1], 'và', list_day_so[0])
